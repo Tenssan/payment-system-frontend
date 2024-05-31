47ffca0cc9e34e72c527a414b068e477f33a8623
Carale1601 <carlos.ibanez@alumnos.ucn.cl>
2024-05-31 12:50:32 -0400

    token+storage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import StandardLandingPage from './pages/standard-user/landing/page';
+import { useRouter } from 'next/router';
 
 const Home: React.FC = () => {
+  
+
   return (
     <StandardLandingPage />
   );
--- app/pages/standard-user/landing/page.tsx
+++ app/pages/standard-user/landing/page.tsx
@@ -1,7 +1,26 @@
+'use client'
+
+import { useRouter, useSearchParams, useSelectedLayoutSegment } from "next/navigation";
+import { useEffect, useState } from "react";
+
+
 const StandardLandingPage: React.FC = () => {
+    const searchParams = useSearchParams();
+    const token = searchParams.get('token');
+    const [storedToken, setStoredToken] = useState<string | null>(null);
+    useEffect(() => {
+        if (token) {
+          localStorage.setItem('token', token);
+          setStoredToken(token);
+        } else {
+          const savedToken = localStorage.getItem('token');
+          setStoredToken(savedToken);
+        }
+      }, [token]);
     return (
         <div>
             <h1>Landing Page</h1>
+            <p>Your token is: {storedToken}</p>
         </div>
     );
 };
